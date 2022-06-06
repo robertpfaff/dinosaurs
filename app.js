a43d2d531e76a6160d1f92205383af0a544c0b08
robertpfaff <42342840+robertpfaff@users.noreply.github.com>
2022-06-06 10:46:25 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+/**
+* @description Class constructor
+* @constructor Dinosaurs
+* @param {Udacity Project 1 Intermediate Javascript} title
+* @param {Robert Pfaff} author
+*/
+
 class Dinosaur {
     constructor(species, height, weight, image, when, where, diet, facts, fun_fact) {
         this.species = species;
@@ -7,23 +14,30 @@ class Dinosaur {
         this.when = when;
         this.where = where;
         this.diet = diet;
+        // Each dinosaur has an array of six facts, primed with when and where.
         facts = [`The ${this.species} lived in ${this.where}`, `The ${this.species} lived during the ${this.when} period.`];
         this.facts = facts;
+        // Fun fact variable is used to return randomized fact from facts array.
+        // See randomFact function on line 158 below.
         this.fun_fact = fun_fact;
-        this.weightComp = function (user) { // Remember to format numbers.
+
+        // 1) Weight comparison function
+        this.weightComp = function (user) {
             if (user.weight < this.weight) {
                 let ratio = parseFloat((this.weight - user.weight) / user.weight) * 100;
                 let number = Math.round(ratio);
-                let result = `The ${this.species} was ${number} % heavier than you.`;
-                this.facts.push(result)
+                // Use comma separators if in thousands
+                let result = `The ${this.species} was ${number.toLocaleString('en-US')}% heavier than you.`;
+                this.facts.push(result);
                 return result;
             } else {
                 let ratio = parseFloat((user.weight - this.weight) / this.weight) * 100;
                 let number = Math.round(ratio);
-                let result = `The ${this.species} was ${number} % lighter than you.`;
-                this.facts.push(result)
+                let result = `The ${this.species} was ${number.toLocaleString('en-US')}% lighter than you.`;
+                this.facts.push(result);
                 return result;
         }};
+        // 2) Height comparison functions
         this.heightComp = function (user) {
             if (user.height < this.height) {
                 let difference = parseFloat(this.height - user.height);
@@ -38,6 +52,7 @@ class Dinosaur {
                 this.facts.push(result)
                 return result;
         }};
+        // 3) Diet comparison function
         this.dietComp = function(user) {
             switch(this.diet === user.diet) {
                 case true:
@@ -49,8 +64,9 @@ class Dinosaur {
                     this.facts.push(result2);
                     return result2;
                 default:
-                    return `Sorry. Does not compute.`;                
+                    return `Sorry. Does not compute.`;
         }};
+        // 4) Diet comparison function
         this.nameComp = function (user) {
             let user_name = String(user.name).trim();
             let dino_name = String(this.name).trim();
@@ -71,9 +87,13 @@ class Dinosaur {
                 this.facts.push(name_fact);
                 return name_fact;
         }};
-        
-    }        
+    }
 }
+
+/**
+* @description Class constructor
+* @constructor Human Being
+*/
 
 class HumanBeing {
     constructor(name, species, height, weight, image, diet) {
@@ -83,58 +103,61 @@ class HumanBeing {
         this.weight = weight;
         this.image = image;
         this.diet = diet;
-    }    
+    }
 }
- 
-// IIFE, so we don't leak dinoData to the outside world.
+/**
+* @description IIFE wrapper
+// Code in IIFE wrapper, so we don't leak data to outside world.
 // I'm assuming the class definitions SHOULD be global,
 // so they're not in the IIFE.
+*/
 
 (() => {
     let dinoData = [];
-    // fetch dino data and make each dino
+    // Fetch dino data and mint each dino
     fetch("./dino.json")
         .then(response => response.json())
         .then(json_data => {
-            // mapping dino objects from json data    
+            // mapping dino objects from json data
             dinoData = json_data.Dinos.map(eachDinos =>
                 new Dinosaur(eachDinos.species, eachDinos.height, eachDinos.weight,
                     eachDinos.image, eachDinos.when, eachDinos.where, eachDinos.diet, eachDinos.facts));
- 
-            console.log('dinoData Array Contents:');
-            console.log(dinoData);
-            console.log(Array.isArray(dinoData));
-        });        
- 
-    // collect form data and make user
+    });
+
+    // Collect form data and make user after button click.
+
     let button = document.getElementById('btn');
     button.addEventListener('click', (e) => {
         e.preventDefault();
         let name = document.getElementById('name').value;
-        let species = "Human Being" 
-        let height_feet = Number(document.getElementById('feet').value) * 12; 
-        let height_inches = Number(document.getElementById('inches').value); 
+        if (name.value = "" || null) {
+            messages.push("Name is a required field.")
+        }
+        let species = "Human Being"
+        let height_feet = Number(document.getElementById('feet').value) * 12;
+        let height_inches = Number(document.getElementById('inches').value);
         let height = height_feet + height_inches;
-        let weight = Number(document.getElementById('weight').value); 
+        let weight = Number(document.getElementById('weight').value);
         let image = "./images/human.png";
         let diet = document.getElementById('diet').value;
-        let facts = []; 
-    
+        let facts = [];
+
+        // After form validation mint new human
         let user = new HumanBeing(name, species, height, weight, image, diet, facts);
-        
-        console.log(dinoData); // should print dinos and a human
-        console.log(typeof dinoData)
-        
-        // fill facts array for each dino with six facts
+
+        // Call weightComp method
         for (let i = 0; i < dinoData.length; i++) {
             weight_data = dinoData[i].weightComp(user);
         }
+        // Call heightComp method
         for (let i = 0; i < dinoData.length; i++) {
             height_data = dinoData[i].heightComp(user);
         }
+        // Perform diet comparison
         for (let i = 0; i < dinoData.length; i++) {
             diet_data = dinoData[i].dietComp(user);
         }
+        // Perform name comparison
         for (let i = 0; i < dinoData.length; i++) {
             name_data = dinoData[i].nameComp(user);
         }
@@ -142,29 +165,34 @@ class HumanBeing {
         for (let i = 0; i < dinoData.length; i++) {
             if (dinoData[i].species == "Pigeon") {
             dinoData[i].facts = ["All birds are dinosaurs.", "All birds are dinosaurs.","All birds are dinosaurs.","All birds are dinosaurs.","All birds are dinosaurs.","All birds are dinosaurs." ];          
-        }}        
-        // Remove form, w/o deleting dinoData.
+        }};
+        // Remove form on click, w/o deleting dinoData.
         let node = document.getElementById("dino-compare");
         if (node.parentNode) {
             node.parentNode.removeChild(node);
         }
 
-        // Make an object of randomized fun fact
-        // Randomized when browser refreshed
-        
+        // Called on line 209-210 below
+        // Function assigns one random fact to fun_fact
+        // Randomized after browser refreshed
+        // And new user info entered
+
         function randomFact(arr) {
             fun_fact = arr[Math.floor(Math.random() * 6)];
-            console.log(fun_fact);
             return fun_fact;
         }
 
-        //Shuffle dinoData array before inserting user at four.
+        // Shuffle dinoData array before inserting user at four.
+        // Dino images appear in random location on grid
+        // While person image stays in place
+
         dinoData.sort(function() { return 0.5 - Math.random() });
-        
-        //Splice human/user into dinoData array
+
+        //Splice human/user into dinoData array in middle
         dinoData.splice(4, 0, user);
 
-        // Now we arrange shuffleds tiles on grid.
+        // Arrange shuffled tiles on grid.
+        // Place fourth tile in center square
 
         for (let i = 0; i < 9; i++) {
             if (i === 4){
@@ -186,4 +214,6 @@ class HumanBeing {
         }
 })
 
+// End of IIFE wrapper
+// See line 107-108
 })();
